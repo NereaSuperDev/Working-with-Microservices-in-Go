17556e433a43ea85ce5e454826e34ece06abe968
NereaSuperDev <nerea.almiron1123@gmail.com>
2022-09-17 19:48:35 +0430

    grpc

--- broker-service/cmd/api/routes.go
+++ broker-service/cmd/api/routes.go
@@ -15,7 +15,7 @@ func (app *Config) routes() http.Handler {
 	mux.Use(cors.Handler(cors.Options{
 		AllowedOrigins:   []string{"https://*", "http://*"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"Accept", "Autherization", "Content-Type", "X-CSRF-Token"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
 		ExposedHeaders:   []string{"Link"},
 		AllowCredentials: true,
 		MaxAge:           300,
@@ -25,7 +25,9 @@ func (app *Config) routes() http.Handler {
 
 	mux.Post("/", app.Broker)
 
+	mux.Post("/log-grpc", app.LogViaGRPC)
+
+	mux.Post("/handle", app.HandleSubmission)
+
 	return mux
 }
-
-
