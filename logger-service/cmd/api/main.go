6866cb3a294fb998b473855e39bd01934be47a08
NereaSuperDev <nerea.almiron1123@gmail.com>
2022-09-17 18:17:55 +0430

    communication btw services RPC

--- logger-service/cmd/api/main.go
+++ logger-service/cmd/api/main.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"log"
 	"log-service/data"
+	"net"
 	"net/http"
+	"net/rpc"
 	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
@@ -48,6 +50,8 @@ func main() {
 		Models: data.New(client),
 	}
 
+	go app.rpcListen()
+
 	// start web server
 	// go app.serve()
 	log.Println("Starting service on port", webPort)
@@ -59,6 +63,24 @@ func main() {
 	err = srv.ListenAndServe()
 	if err != nil {
 		log.Panic()
+	}
+
+}
+
+func (app *Config) rpcListen() error {
+	log.Println("Starting RPC server on port ", rpcPort)
+	listen, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%s", rpcPort))
+	if err != nil {
+		return err
+	}
+	defer listen.Close()
+
+	for {
+		rpcConn, err := listen.Accept()
+		if err != nil {
+			continue
+		}
+		go rpc.ServeConn(rpcConn)
 	}
 
 }
